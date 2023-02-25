5ac27d0b3be4ba8b2997f67307d7cf81a74229aa
Uthman Ahmed <70240388+Hutty10@users.noreply.github.com>
2023-02-25 08:04:17 +0100

    started account

--- lib/services/account_service.dart
+++ lib/services/account_service.dart
@@ -1,3 +1,11 @@
-class AccountService{
-  
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AccountService {
+  final User? _user = FirebaseAuth.instance.currentUser;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Future accountDetails()async{
+    
+  }
 }
